87ec5a35764dda1eb5f6b42301805ac7c756f454
jKodrum <jkodrum@gmail.com>
2014-12-14 02:51:23 +0800

    add some test data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,23 @@
 class DashboardController < ApplicationController
   def index
+    @ip = %(10.10.220.233)
+    @cmd = %(/cmd,/ck6fup6/fileBrowser_main/browse?fstatus=waiting&view=tree)
+    @path = %(&path=%2F&share=public)
+    @url = "http://" << @ip << @cmd << @path
+    cookies[:authtok] = {
+      value: "GQYlRjwQlKEwKPEMALtJRRSn9LJUijxMWnsOykzu+ckK7-6QV-3FUmpRUAYQfQuf"
+    }
+    cookies[:target] = {
+      value: "butter"
+      :domain  => "google.com"
+    }
+
+    require 'open-uri'
+    @body = open(@url).read
+
+  end
+
+  def NAS_login
+    redirect_to user_session_path(:resource_name)
   end
 end
