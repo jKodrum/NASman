df155dec282c1e42d2f8aeb712a6646d02ceb2e2
jKodrum <jkodrum@gmail.com>
2014-12-13 19:02:47 +0800

    reindex email by email and provider in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable ##, :validatable
+         :recoverable, :rememberable, :trackable,  request_keys: [:provider] ##, :validatable
   devise :omniauthable, omniauth_providers: [:facebook, :dropbox]
 
+  before_create :set_provider
+
   validates :name, presence: true
-  validates :email, uniqueness: {scope: :provider}
+  #validates :email, uniqueness: {scope: :provider}
 
 
   def self.from_omniauth(auth)
@@ -27,4 +29,10 @@ class User < ActiveRecord::Base
   def facebook
     @facebook ||= Koala::Facebook::API.new(oauth_token) 
   end 
+
+
+  private
+  def set_provider
+    self.provider ||= "NAS"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141213065739) do
+ActiveRecord::Schema.define(version: 20141213105316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20141213065739) do
     t.datetime "oauth_expires_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email", "provider"], name: "index_users_on_email_and_provider", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
